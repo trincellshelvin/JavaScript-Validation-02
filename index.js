130b839a233444965e11ff12054744719c4df975
trincellshelvin <trincellshelvin@gmail.com>
2024-08-07 00:37:21 +0000

    completed <5>7 functions

---
+++ index.js
@@ -0,0 +1,24 @@
+// name is between 5 and 7 characters (inclusive): "will encounter a once-in-a-lifetime oppurtunity"
+// first letter is 'R': "will be rich"
+// Name contains an 'i': "will fall in love this week"
+
+let myName;
+myName = "Trin";
+let lengthOfName = myName.length;
+if (lengthOfName<5){
+    console.log("You will marry soon!")
+}
+
+let name;
+name = "Trincell";
+let lengthofName = name.length;
+if (lengthofName>7){
+    console.log("You will marry late in life!")
+}
+
+let Name;
+Name = "Trincell";
+let lengthName = Name.length;
+if (lengthName<5 && lengthName>7){
+    console.log("You will encounter a once-in-a-lifetime opportunity")
+}
