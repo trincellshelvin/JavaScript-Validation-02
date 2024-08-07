190087b32eb957750f9696a499419bd57cf98b99
trincellshelvin <trincellshelvin@gmail.com>
2024-08-07 01:53:05 +0000

    completed first letter

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// name is between 5 and 7 characters (inclusive): "will encounter a once-in-a-lifetime oppurtunity"
-// first letter is 'R': "will be rich" https://www.w3schools.com/jsref/jsref_charat.asp
 // Name contains an 'i': "will fall in love this week"
 
 let myName;
@@ -9,15 +7,20 @@ if (lengthOfName<5){
     console.log("You will marry soon!")
 }
 
-let name;
-name = "Trincell";
-let lengthofName = name.length;
+myName = "Trincell";
+let lengthofName = myName.length;
 if (lengthofName>7){
     console.log("You will marry late in life!")
 }
 
-let Name = "Trincel";
-let lengthName = Name.length;
-if (lengthName >= 5 && lengthName <= 7){
+myName = "Trincel";
+let lengthName = myName.length;
+if (lengthName >= 5 && lengthOfName <= 7){
     console.log("You will encounter a once-in-a-lifetime opportunity")
+}
+
+let Character = myName[0];
+console.log("first letter is " + Character);
+if(Character === "T") {
+    console.log("You will be rich!")
 }
