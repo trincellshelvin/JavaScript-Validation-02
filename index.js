46aa6ae65901db18259bf274e2e4867197e2abf0
trincellshelvin <trincellshelvin@gmail.com>
2024-08-07 01:07:15 +0000

    completed between function

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // name is between 5 and 7 characters (inclusive): "will encounter a once-in-a-lifetime oppurtunity"
-// first letter is 'R': "will be rich"
+// first letter is 'R': "will be rich" https://www.w3schools.com/jsref/jsref_charat.asp
 // Name contains an 'i': "will fall in love this week"
 
 let myName;
@@ -16,9 +16,8 @@ if (lengthofName>7){
     console.log("You will marry late in life!")
 }
 
-let Name;
-Name = "Trincell";
+let Name = "Trincel";
 let lengthName = Name.length;
-if (lengthName<5 && lengthName>7){
+if (lengthName >= 5 && lengthName <= 7){
     console.log("You will encounter a once-in-a-lifetime opportunity")
 }
