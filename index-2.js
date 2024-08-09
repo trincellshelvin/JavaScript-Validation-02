c078ad6368f9917b7012a25a1feb8147c4de504e
trincellshelvin <trincellshelvin@gmail.com>
2024-08-09 02:31:52 +0000

    removed comments and check console log

--- index-2.js
+++ index-2.js
@@ -1,12 +1,3 @@
-//Name is longer than 7 characters: "will marry late in life" function willMarryLate-done
-//Name is shorter than 5 characters: "will marry within a year." function willMarryEarly-done
-//name is between 5 and 7 characters (inclusive): "will encounter a once-in-a-lifetime oppurtunity" 
-//function onceInALifeTime
-//first letter is 'R': "will be rich" function willBeRich-done
-//Name contains an 'i': "will fall in love this week" function willFallInLove-done
-//Note: These function will take the name (a string) as an ARGUMENT, and return a boolean
-
-console.log("Hello")
 function willFallInLove(myName) {
     let x = myName.includes("i");
     if(x){
@@ -48,7 +39,7 @@ function onceInALifeTime(myName){
         return false
     }
 }
-//greater than 7
+
 function tellaFortune(myName){
     let n = willMarryLate(myName);
     if(n){
@@ -56,26 +47,26 @@ function tellaFortune(myName){
     } else{
         console.log(`${myName} You will not marry late...`);
     }
-    //has i
+    
     let m = willFallInLove(myName);
     if(m){
         console.log("You will meet a mysterious stranger before the week is over.");
     }
-    //less than 5
+    
     let o = willMarryEarly(myName);
     if(o){
         console.log(`${myName} you will marry within a year!`)
     } else{
         console.log(`${myName} you will not marry within a year!`)
     }
-    // starts with R
+    
     let p = willBeRich(myName);
     if(p){
         console.log(`${myName} you will be rich!`)
     } else{
         console.log(`${myName} you will not be rich`)
     }
-    //greater than 5 less than 7
+
     let q =onceInALifeTime(myName);
     if(q){
         console.log(`${myName} you will encounter a once-in-a-lifetime opportunity`)
